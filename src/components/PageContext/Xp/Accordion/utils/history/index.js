5271dbd559d2112cc14885d6cb62ed48b331227a
Piotr Jankowski <piotr.jankowski87@gmail.com>
2019-11-24 01:40:09 +0100

    Update experience entries

--- src/components/PageContext/Xp/Accordion/utils/history/index.js
+++ src/components/PageContext/Xp/Accordion/utils/history/index.js
@@ -1,42 +1,52 @@
 export default [
   {
-    timespan: '11.2016 - present',
+    timespan: '05.2019 - present',
+    company: 'Cisco',
+    jobTitle: 'Software Engineer',
+    description: `Full-stack development of innovative projects meant to disrupt the tech world. Technologies used:
+      TypeScript, Yarn, Vue.js, SASS, React, Docker, Node.js, Express, PostgreSQL, Python, Electron`
+  },
+  {
+    timespan: '11.2016 - 03.2019',
     company: 'Toptal',
     jobTitle: 'Front-end Developer',
-    description: `Currently working on front-end for core Toptal platform. I also worked as a lead front-end developer
-      on Collabshot and as a maintainer of TopTracker`
+    description: `Front-end development of the core Toptal platform. Also fulfilled the role of the lead front-end
+      developer of Collabshot and as a maintainer of TopTracker. Technologies used: TypeScript, JavaScript, Yarn, SASS,
+      React, Node.js, PostgreSQL.`
   },
   {
     timespan: '06.2014 - 10.2016',
     jobTitle: 'Private Projects Development',
-    description: 'Created this interactive resume, Space Rabbit Unity3D mobile game and Tiny Life Counter hybrid application'
+    description: `Created this interactive resume, Space Rabbit Unity3D mobile game and Tiny Life Counter hybrid
+      application.`
   },
   {
     timespan: '04.2014 - 06.2016',
     company: 'Currency One',
     jobTitle: 'Front-end Developer',
-    description: 'Wide front-end development using Javascript, CSS, HTML, AngularJS and Backbone.js'
+    description: 'Front-end development of web applications using Javascript, CSS, HTML, AngularJS and Backbone.js'
   },
   {
     timespan: '05.2013 - 03.2014',
     company: 'Roche',
     jobTitle: 'Front-end Developer',
-    description: `Development of variety of projects using JavaScript, jQuery, Backbone.js, AngularJS, PHP, Symfony 2.x,
-      MySQL and elements of Java.`
+    description: `Full-stack development of applications using JavaScript, jQuery, Backbone.js, AngularJS, PHP,
+      Symfony 2.x, MySQL and elements of Java.`
   },
   {
     timespan: '07.2011 - 10.2012',
     company: 'WebDoctor',
     jobTitle: 'Programmer',
-    description: `Development and maintenance of internal company's applications and external websites. Technologies
-      used: PHP, Symfony Framework 2.x, JavaScript with jQuery and MySQL database. Operating system used at work: Linux.`
+    description: `Full-stack development and maintenance of internal company's applications and external websites.
+      Technologies used: PHP, Symfony Framework 2.x, JavaScript with jQuery and MySQL database. Operating system used
+      at work: Linux.`
   },
   {
     timespan: '10.2011 - 07.2012',
     company: 'Status-C',
     jobTitle: 'PHP Programmer',
-    description: `Development of web-based applications. Technologies used: PHP, Zend Framework, JavaScript, jQuery,
-      MySQL, small bit of C# (Unity3D). Main operating system used at work: Linux.`
+    description: `Full-stack development of web applications. Technologies used: PHP, Zend Framework, JavaScript,
+      jQuery, MySQL, small bit of C# (Unity3D). Main operating system used at work: Linux.`
   },
   {
     timespan: '07.2011 - 10.2012',
